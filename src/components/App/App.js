e36f7ecd6cf9dff984ff09a90cf2158160e7648e
Sam Jones <sam.jones494@googlemail.com>
2020-03-03 10:41:27 +0000

    retrieve articles from api on page load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 import ReadingList from '../ReadingList/ReadingList';
 
-function App() {
-  return (
-    <div className="App" data-test="component-app">
-      <ReadingList articles={ [] } />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      articles: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/api/articles')
+    .then((response) => {
+      this.setState({ articles: response.data })
+    })
+    .catch((error) => {
+    });
+  }
+
+  render() {
+    const { articles } = this.state;
+
+    return (
+      <div className="App" data-test="component-app">
+        <ReadingList articles={ articles } />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import axios from 'axios';
+import moxios from 'moxios';
 
 import App from './App';
 import ReadingList from '../ReadingList/ReadingList';
@@ -15,5 +17,33 @@ describe("App", () => {
     const wrapper = shallow(<App />);
     const readingListComponent = wrapper.find(ReadingList);
     expect(readingListComponent.exists()).toBe(true);
+  });
+
+  describe('makes a request to the pocketClone api', () => {
+    let wrapper
+
+    beforeEach(() => {
+      moxios.install();
+      wrapper = mount(<App />);
+    });
+
+    afterEach(() => {
+      moxios.uninstall();
+    });
+
+    describe('on sucess', () => {
+      it('retrieves a list of saved articles on page load and stores them in state', (done) => {
+        moxios.wait(() => {
+          const request = moxios.requests.mostRecent();
+          request.respondWith({
+            status: 200,
+            response: [{url: 'example.com'}, {url: 'www.anotherwebsite.com'}]
+          }).then(() => {
+            expect(wrapper.state().articles.length).toEqual(2)
+            done()
+          });
+        });
+      });
+    });
   });
 })
